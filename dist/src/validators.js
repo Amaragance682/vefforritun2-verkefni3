35ce51d2b8b80c3e6ae6671b84c44bc813053415
Amaragance682 <esdeathxoli@gmail.com>
2025-03-06 21:27:41 +0000

    fuckit

---
+++ dist/src/validators.js
@@ -0,0 +1,54 @@
+import { z } from "zod";
+import { filterXSS } from "xss";
+export function sanitizeJSON(input) {
+    if (typeof input === "string") {
+        return filterXSS(input);
+    }
+    if (Array.isArray(input)) {
+        return input.map(sanitizeJSON);
+    }
+    if (typeof input === "object" && input !== null) {
+        const sanitizedObject = {};
+        for (const [key, value] of Object.entries(input)) {
+            sanitizedObject[key] = sanitizeJSON(value);
+        }
+        return sanitizedObject;
+    }
+    return input;
+}
+const SlugSchema = z
+    .string()
+    .min(1, "slug must be at least one letter")
+    .max(1024, "slug cannot be more than 1024 letters");
+const CategorySchema = z.object({
+    title: z
+        .string()
+        .min(1, "category cannot have an empty name")
+        .max(1024, "category cant have a name longer than 1024 characters"),
+});
+const QuestionSchema = z.object({
+    question: z
+        .string()
+        .min(1, "question must be at least one character")
+        .max(2048, "question cannot be longer than 2048 characters"),
+    categoryId: z.number(),
+    correctAnswer: z
+        .number()
+        .min(1, "correct answer cant be outside the range 1-4")
+        .max(4, "correct answer cant be outside the range 1-4"),
+    answers: z
+        .array(z
+        .string()
+        .min(1, "answer cannot be empty")
+        .max(2048, "answer cant be longer than 2048 characters"))
+        .length(4, "answers must be four"),
+});
+export function validateSlug(slug) {
+    return SlugSchema.safeParse(slug);
+}
+export function validateCategory(category) {
+    return CategorySchema.safeParse(category);
+}
+export function validateQuestion(question) {
+    return QuestionSchema.safeParse(question);
+}
