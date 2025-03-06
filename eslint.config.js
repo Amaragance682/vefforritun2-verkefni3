f374da9e1466f0d7e56f875f362576dc996b923e
Amaragance682 <esdeathxoli@gmail.com>
2025-03-06 21:09:29 +0000

    fixed eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,26 @@
-import tsParser from '@typescript-eslint/parser';
-import globals from 'globals';
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
 
 export default [
-    {
-        files: ['./src/**/*.ts'],
-        languageOptions: {
-            parser: tsParser,
-            globals: globals.browser
-        }
-    }
-]
+  {
+    files: ["**/*.ts", "**/*.tsx"], 
+    ignores: ["node_modules/", "dist/", "build/"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
